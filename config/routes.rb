d993f2f4b7c9ee69a604b1e71b145dfc8d9a71ab
atomi3973 <atomiroi3973@gmail.com>
2026-03-01 20:20:32 +0900

    レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root "home#index"
+  get 'terms', to: 'home#terms'
+  get 'privacy', to: 'home#privacy'
   resources :schedules
   post "/line/callback", to: "line_bot#callback"
   post '/scheduler/execute_notifications', to: 'scheduler#execute'
